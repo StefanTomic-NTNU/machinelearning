90c191558d5211a19f57cdfe55a9eb76d09a98dc
Stefan Djordje Tomic <stefandt@stud.ntnu.no>
2021-09-13 13:34:02 +0200

    Implemented k++

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -10,12 +10,13 @@ import random as r
 
 class KMeans:
     
-    def __init__(self, k=2, stretch=1):
+    def __init__(self, k=2, normalize_points=True, k_means_plus_plus=True):
         # NOTE: Feel free add any hyperparameters
         # (with defaults) as you see fit
+        self.k_means_plus_plus = k_means_plus_plus
+        self.normalize_points = normalize_points
         self.centroids = None
         self.k = k
-        self.stretch = stretch
         self.displacement = None
         self.ratio = None
 
@@ -43,35 +44,61 @@ class KMeans:
         """
         # TODO: Implement
         n = X.shape[0]
-        X = self.normalize(X)
+        if self.normalize_points:
+            X = self.normalize(X)
         X_num = X.to_numpy()
-        # X_num = X.to_numpy()
-        # X_0_mean = np.mean(X_num[:, 0])
-        # X_1_mean = np.mean(X_num[:, 1])
-        # X_normalized = np.copy(X)
-        # X_normalized[:, 0] -= X_0_mean
-        # X_normalized[:, 1] -= X_1_mean
 
-        self.centroids = np.zeros((self.k, 2))
-        for j in range(self.k):
+        if not self.k_means_plus_plus:
+            self.centroids = np.zeros((self.k, 2))
+            for j in range(self.k):
+                index = np.random.choice(n)
+                self.centroids[j, :] = X_num[index]
+        else:
+            # K means++
+            self.centroids = np.zeros((2, 2), dtype='float64')
             index = np.random.choice(n)
-            self.centroids[j, :] = X_num[index]
-        print(self.centroids)
-
+            self.centroids[0, :] = X_num[index]
+            index = np.random.choice(n)
+            self.centroids[1, :] = X_num[index]
+            # print(self.centroids)
+            for _ in range(2, self.k):
+                # print(self.centroids)
+                distances = cross_euclidean_distance(X_num, self.centroids)
+                min_index = np.argmin(distances, axis=1)
+                max_index = None
+                max_distance = 0
+                for i in range(len(min_index)):
+                    dist = distances[i, min_index[i]]
+                    if dist > max_distance:
+                        max_distance = dist
+                        max_index = i
+                self.centroids = np.vstack([self.centroids, X_num[max_index]])
 
         # Assigning of clusters
-        for _ in range(100):
+        for _ in range(20):
+            # print(self.centroids)
             distances = cross_euclidean_distance(X_num, self.centroids)
+            # print(distances)
+
+            # centroid blir ikke assignet til noen punkter
             min_index = np.argmin(distances, axis=1)
 
+            count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             for j in range(self.k):
                 points = []
                 for i in range(n):
                     if min_index[i] == j:
                         points.append(X_num[i, :])
-                self.centroids[j, :] = np.mean(np.array(points, dtype='float'), axis=0)
-        self.centroids *= self.ratio
-        self.centroids += self.displacement
+                        count[j] += 1
+                # print(points)
+                # print(np.mean(np.array(points, dtype='float64'), axis=0))
+                self.centroids[j, :] = np.mean(np.array(points, dtype='float64'), axis=0)
+            # print(count)
+
+        # print(self.centroids)
+        if self.normalize_points:
+            self.centroids *= self.ratio
+            self.centroids += self.displacement
 
 
     def predict(self, X):
@@ -91,12 +118,14 @@ class KMeans:
             could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
         """
         # TODO: Implement
-        self.centroids -= self.displacement
-        self.centroids /= self.ratio
-        X = self.normalize(X)
+        if self.normalize_points:
+            self.centroids -= self.displacement
+            self.centroids /= self.ratio
+            X = self.normalize(X)
         distances = cross_euclidean_distance(X.to_numpy(), self.centroids)
-        self.centroids *= self.ratio
-        self.centroids += self.displacement
+        if self.normalize_points:
+            self.centroids *= self.ratio
+            self.centroids += self.displacement
         return np.argmin(distances, axis=1)
     
     def get_centroids(self):
@@ -214,12 +243,12 @@ def euclidean_silhouette(X, z):
             div = d.shape[1] - int(i == j)
             D[in_cluster_a, j] = d.sum(axis=1) / np.clip(div, 1, None)
     
-    # Intra distance 
+    # Intra distance
     a = D[np.arange(len(X)), z]
+
     # Smallest inter distance 
     inf_mask = np.where(z[:, None] == clusters[None], np.inf, 0)
     b = (D + inf_mask).min(axis=1)
-    
     return np.mean((b - a) / np.maximum(a, b))
 
 
